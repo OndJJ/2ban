cf8a12da2311d228871c838eb8684dd889e246aa
OndJJ <jcjk0302@gmail.com>
2021-08-26 17:42:37 +0900

    use to Transaction

--- pragmatic/likeapp/views.py
+++ pragmatic/likeapp/views.py
@@ -1,6 +1,8 @@
 # Create your views here.
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.core.exceptions import ValidationError
+from django.db import transaction
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404
 from django.urls import reverse
@@ -9,6 +11,18 @@ from django.views.generic import RedirectView
 
 from articleapp.models import Article
 from likeapp.models import LikeRecord
+
+
+@transaction.atomic
+def db_transaction(user, article):
+
+    article.like += 1
+    article.save()
+
+    if LikeRecord.objects.filter(user=user, article=article).exists():
+        raise ValidationError('Like already exists')
+    else:
+        LikeRecord(user=user, article=article).save()
 
 
 @method_decorator(login_required, 'get')
@@ -21,15 +35,11 @@ class LikeArticleView(RedirectView):
         user = self.request.user
         article = get_object_or_404(Article, pk=kwargs['pk'])
 
-        if LikeRecord.objects.filter(user=user, article=article).exists():
+        try:
+            db_transaction(user, article)
+            messages.add_message(self.request, messages.SUCCESS, 'LIKE IT !')
+        except ValidationError:
             messages.add_message(self.request, messages.ERROR, '이미 좋아요를 눌렀습니다.')
             return HttpResponseRedirect(reverse('articleapp:detail', kwargs={'pk': kwargs['pk']}))
-        else:
-            LikeRecord(user=user, article=article).save()
-
-        article.like += 1
-        article.save()
-
-        messages.add_message(self.request, messages.SUCCESS, 'LIKE IT !')
 
         return super(LikeArticleView, self).get(self.request, *args, **kwargs)
