febb40ba6bb98ecd470511763f44e0a1b8f22884
OndJJ <jcjk0302@gmail.com>
2021-08-30 18:20:06 +0900

    set aws / gcp 과정은 인프런 참고

--- pragmatic/articleapp/models.py
+++ pragmatic/articleapp/models.py
@@ -7,12 +7,12 @@ from projectapp.models import Project
 
 class Article(models.Model):
     writer = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='article', null=True)
-    project = models.ForeignKey(Project, on_delete=models.SET_NULL, related_name='article', null=True)
+    project = models.ForeignKey(Project, on_delete=models.SET_NULL, related_name='article', null=True, blank=True) # blank = 입력도 안 받아도 된다.
 
     title = models.CharField(max_length=200, null=True)
-    image = models.ImageField(upload_to='article/', null=False)
+    image = models.ImageField(upload_to='article/', null=True)
     content = models.TextField(null=True)
 
-    created_at = models.DateField(auto_created=True, null=True)
+    created_at = models.DateField(auto_now_add=True) # auto_created=True, null=True
 
     like = models.IntegerField(default=0)
