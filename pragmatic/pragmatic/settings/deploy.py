91efae79c910098abb3122ef9d43651a87457975
OndJJ <jcjk0302@gmail.com>
2021-09-23 17:45:50 +0900

    9/ 21 commit 3

--- pragmatic/pragmatic/settings/deploy.py
+++ pragmatic/pragmatic/settings/deploy.py
@@ -1,21 +1,16 @@
 from .base import *
 
 
-env = environ.Env(
-    # set casting, default value
-    DEBUG=(bool, False)
-)
+def read_secret(secret_name):
+    file = open('/run/secrets/' + secret_name)
+    secret = file.read()
+    secret = secret.rstrip().lstrip()
+    file.close()
+    return secret
 
-# reading .env file
-environ.Env.read_env(
-    env_file=(os.path.join(BASE_DIR, '.env'))
-)
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = read_secret('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -29,8 +24,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'django',
-        'USER': 'django',
-        'PASSWORD': 'ckdwns4028',
+        'USER': read_secret('MARIADB_USER'),
+        'PASSWORD': read_secret('MARIADB_PASSWORD'),
         'HOST': 'mariadb',
         'PORT': '3306',
     }
